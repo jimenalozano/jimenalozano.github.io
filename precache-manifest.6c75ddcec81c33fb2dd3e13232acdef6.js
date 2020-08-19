16f32aa51761d2882b3e2a7326a24d02a431ec66
jimenalozano <jilozano@itba.edu.ar>
2020-08-18 21:15:17 -0300

    Updates

---
+++ precache-manifest.6c75ddcec81c33fb2dd3e13232acdef6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "785b886990b3518aaf9ac08841884b19",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8e548da5bda14a70e12",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "21a478aac853d8038459",
+    "url": "/static/js/2.6457aad2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6457aad2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8e548da5bda14a70e12",
+    "url": "/static/js/main.0ba93d85.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "5c36924c5cfb170de196e47596e91d2e",
+    "url": "/static/media/profile.5c36924c.jpg"
+  }
+]);
